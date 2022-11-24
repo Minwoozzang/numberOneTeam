fa9d993e98f00a11866027b3774f57f94b2cdfe2
Kim Minwoo <kminu0819@naver.com>
2022-11-24 14:43:52 +0900

    11/24 민우

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-import { handleAuth, onToggle, logout } from './pages/auth.js';
-import { changeProfile, onFileChange, profileModal } from './pages/profile.js';
-import { socialLogin } from './pages/auth.js';
-import { handleLocation, goToProfile, goToMyPage } from './router.js';
-import { authService } from './firebase.js';
+import { handleAuth, onToggle, logout } from "./pages/auth.js";
+import { changeProfile, onFileChange, profileModal } from "./pages/profile.js";
+import { socialLogin } from "./pages/auth.js";
+import { handleLocation, goToProfile, goToMyPage } from "./router.js";
+import { authService } from "./firebase.js";
 import {
   save_comment,
   update_comment,
@@ -10,29 +10,28 @@ import {
   delete_comment,
   getCommentList,
   getHomePageList,
-  // getQuestionList,
-} from './pages/fanLog.js';
+} from "./pages/fanLog.js";
 
 import {
   save_list,
   // update_list,
   // onEditing_list,
   // delete_list,
-} from './pages/mypage.js';
+} from "./pages/mypage.js";
 
 // url 바뀌면 handleLocation 실행하여 화면 변경
-window.addEventListener('hashchange', handleLocation);
+window.addEventListener("hashchange", handleLocation);
 
 // 모달창 오픈 상태에서 외부 영역 클릭시 모달창 꺼짐
 window.onclick = function (event) {
-  const profilePage = document.querySelector('#profilePage');
+  const profilePage = document.querySelector("#profilePage");
   if (event.target === profilePage) {
-    profilePage.style.display = 'none';
+    profilePage.style.display = "none";
   }
 };
 
 // 첫 랜딩 또는 새로고침 시 handleLocation 실행하여 화면 변경
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   // Firebase 연결상태를 감시
   authService.onAuthStateChanged((user) => {
     // Firebase 연결되면 화면 표시
@@ -40,14 +39,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const hash = window.location.hash;
     if (user) {
       // 로그인 상태이므로 항상 팬명록 화면으로 이동
-      if (hash === '') {
+      if (hash === "") {
         // 로그인 상태에서는 로그인 화면으로 되돌아갈 수 없게 설정
-        window.location.replace('#fanLog');
+        window.location.replace("#fanLog");
       }
     } else {
       // 로그아웃 상태이므로 로그인 화면으로 강제 이동
-      if (hash !== '') {
-        window.location.replace('');
+      if (hash !== "") {
+        window.location.replace("");
       }
     }
   });
@@ -68,9 +67,7 @@ window.delete_comment = delete_comment;
 window.profileModal = profileModal;
 window.getCommentList = getCommentList;
 window.getHomePageList = getHomePageList;
-// window.goToMyPage = goToMyPage;
 window.save_list = save_list;
 // window.update_list = update_list;
 // window.onEditing_list = onEditing_list;
 // window.delete_list = delete_list;
-// window.getQuestionList = getQuestionList;
