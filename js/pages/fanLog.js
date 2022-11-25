d7e9df5b3ca19ce54c46932324888e7b2716bb50
Hoesu Ji <newasborn@gmail.com>
2022-11-26 01:40:16 +0900

    메인, 마이 페이지 네비게이션/마이 페이지 이미지 추가

--- js/pages/fanLog.js
+++ js/pages/fanLog.js
@@ -7,21 +7,32 @@ import {
   orderBy,
   query,
   getDocs,
-} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
-import { dbService, authService } from "../firebase.js";
+} from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js';
+import { dbService, authService } from '../firebase.js';
 
-let selectedDate = "today";
-let comments = "";
+let selectedDate = 'today';
+let comments = '';
 
 export const save_comment = async (event) => {
   event.preventDefault();
-  console.log(selectedDate);
-  const commentInput = document.getElementById("commentId");
-  const commentIntro = document.getElementById("commentIntroduceId");
-  const comment = document.getElementById("comment");
-  if (selectedDate === "yesterday") comments = "comment1";
-  else if (selectedDate === "today") comments = "comment2";
-  else comments = "comment3";
+  // <<<<<<< HEAD
+  //   console.log(selectedDate);
+
+  //   const commentInput = document.getElementById('commentId');
+  //   const commentIntro = document.getElementById('commentIntroduceId');
+  //   const comment = document.getElementById('comment');
+  //   if (selectedDate === 'yesterday') comments = 'comment1';
+  //   else if (selectedDate === 'today') comments = 'comment2';
+  //   else comments = 'comment3';
+  // =======
+
+  const commentInput = document.getElementById('commentId');
+  const commentIntro = document.getElementById('commentIntroduceId');
+  const comment = document.getElementById('comment');
+  if (selectedDate === 'yesterday') comments = 'comment1';
+  else if (selectedDate === 'today') comments = 'comment2';
+  else comments = 'comment3';
+  // >>>>>>> 757ecc7deca7399517d81a677e79f2ac694a55a1
   const { uid, photoURL, displayName } = authService.currentUser;
   try {
     await addDoc(collection(dbService, comments), {
@@ -33,9 +44,11 @@ export const save_comment = async (event) => {
       plusCounter: 0,
       minusCounter: 0,
       content: commentInput.value,
+      likeButton: '',
+      hateButton: '',
       intro: commentIntro.value,
     });
-    comment.value = "";
+    comment.value = '';
     getCommentList(selectedDate);
   } catch (error) {
     alert(error);
@@ -45,16 +58,16 @@ export const save_comment = async (event) => {
 export const onEditing = (event) => {
   // 수정버튼 클릭
   event.preventDefault();
-  const udBtns = document.querySelectorAll(".editBtn, .deleteBtn");
-  udBtns.forEach((udBtn) => (udBtn.disabled = "true"));
+  const udBtns = document.querySelectorAll('.editBtn, .deleteBtn');
+  udBtns.forEach((udBtn) => (udBtn.disabled = 'true'));
 
   const cardBody = event.target.parentNode.parentNode;
   const commentText = cardBody.children[0].children[0];
   const commentInputP = cardBody.children[0].children[1];
 
-  commentText.classList.add("noDisplay");
-  commentInputP.classList.add("d-flex");
-  commentInputP.classList.remove("noDisplay");
+  commentText.classList.add('noDisplay');
+  commentInputP.classList.add('d-flex');
+  commentInputP.classList.remove('noDisplay');
   commentInputP.children[0].focus();
 };
 
@@ -65,10 +78,10 @@ export const update_comment = async (event) => {
 
   const parentNode = event.target.parentNode.parentNode;
   const commentText = parentNode.children[0];
-  commentText.classList.remove("noDisplay");
+  commentText.classList.remove('noDisplay');
   const commentInputP = parentNode.children[1];
-  commentInputP.classList.remove("d-flex");
-  commentInputP.classList.add("noDisplay");
+  commentInputP.classList.remove('d-flex');
+  commentInputP.classList.add('noDisplay');
 
   const commentRef = doc(dbService, comments, id);
   try {
@@ -82,7 +95,7 @@ export const update_comment = async (event) => {
 export const delete_comment = async (event) => {
   event.preventDefault();
   const id = event.target.name;
-  const ok = window.confirm("삭제하시겠습니까?");
+  const ok = window.confirm('삭제하시겠습니까?');
   if (ok) {
     try {
       await deleteDoc(doc(dbService, comments, id));
@@ -95,22 +108,23 @@ export const delete_comment = async (event) => {
 
 export const commentLike = async (event) => {
   event.preventDefault();
-  // <------------ 나중에 disabled 할수도 있어서 남겨둔 거니 지우지 마세요!! -------------->
-  // const udBtns = document.querySelector(".button1");
-  // console.log(udBtns);
-  // udBtns.disabled = "true";
   const id = event.target.id;
-  console.log(event.target);
   const input1 = document.getElementById(`input1${id}`);
-  let like = Number(input1.value); // 0
+  let like = Number(input1.value);
   if (input1.id === `input1${id}`) {
-    console.log(id);
     like++;
   }
 
+  const uid = authService.currentUser.uid;
   const commentRef = doc(dbService, comments, id);
   try {
     await updateDoc(commentRef, { plusCounter: like });
+  } catch (error) {
+    alert(error);
+  }
+  const commentRef1 = doc(dbService, comments, id);
+  try {
+    await updateDoc(commentRef1, { likeButton: uid });
     getCommentList(selectedDate);
   } catch (error) {
     alert(error);
@@ -123,13 +137,19 @@ export const commentHate = async (event) => {
   const input2 = document.getElementById(`input2${id}`);
   let like = Number(input2.value);
   if (input2.id === `input2${id}`) {
-    console.log(id);
     like++;
   }
-
+  event.target.disabled = true;
+  const uid = authService.currentUser.uid;
   const commentRef = doc(dbService, comments, id);
   try {
     await updateDoc(commentRef, { minusCounter: like });
+  } catch (error) {
+    alert(error);
+  }
+  const commentRef1 = doc(dbService, comments, id);
+  try {
+    await updateDoc(commentRef1, { hateButton: uid });
     getCommentList(selectedDate);
   } catch (error) {
     alert(error);
@@ -138,28 +158,29 @@ export const commentHate = async (event) => {
 
 export const getCommentList = async (time) => {
   let cmtObjList = [];
-  if (selectedDate === "yesterday") {
-    comments = "comment1";
+  if (selectedDate === 'yesterday') {
+    comments = 'comment1';
     getQuestionList(0);
+
     getQuestionIntroduce(0);
-  } else if (selectedDate === "today") {
-    comments = "comment2";
+  } else if (selectedDate === 'today') {
+    comments = 'comment2';
     getQuestionList(1);
     getQuestionIntroduce(1);
   } else {
-    comments = "comment3";
+    comments = 'comment3';
     getQuestionList(2);
     getQuestionIntroduce(2);
   }
-  if (time === "yesterday") {
-    console.log(comments);
+
+  if (time === 'yesterday') {
     const q = query(
       collection(dbService, comments),
-      orderBy("createdAt", "desc")
+      orderBy('createdAt', 'desc')
     );
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
-      if (typeof doc.data().createdAt !== "string") {
+      if (typeof doc.data().createdAt !== 'string') {
         const commentObj = {
           id: doc.id,
           ...doc.data(),
@@ -167,11 +188,11 @@ export const getCommentList = async (time) => {
         cmtObjList.push(commentObj);
       }
     });
-  } else if (time === "today") {
+  } else if (time === 'today') {
     console.log(comments);
     const q = query(
       collection(dbService, comments),
-      orderBy("createdAt", "desc")
+      orderBy('createdAt', 'desc')
     );
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
@@ -181,24 +202,31 @@ export const getCommentList = async (time) => {
       };
       cmtObjList.push(commentObj);
     });
-  } else if (time === "tomorrow") {
-    const q = query(
-      collection(dbService, comments),
-      orderBy("createdAt", "desc")
-    );
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      const commentObj = {
-        id: doc.id,
-        ...doc.data(),
-      };
-      cmtObjList.push(commentObj);
-    });
+    // <<<<<<< HEAD
+    //   } else if (time === 'tomorrow') {
+    //     const q = query(
+    //       collection(dbService, comments),
+    //       orderBy('createdAt', 'desc')
+    //     );
+    //     const querySnapshot = await getDocs(q);
+    //     querySnapshot.forEach((doc) => {
+    //       const commentObj = {
+    //         id: doc.id,
+    //         ...doc.data(),
+    //       };
+    //       cmtObjList.push(commentObj);
+    //     });
+    //   }
+    //   const commnetList = document.getElementById('comment-list');
+    //   const currentUid = authService.currentUser.uid;
+
+    //   commnetList.innerHTML = '';
+    // =======
+  } else if (time === 'tomorrow') {
   }
-  const commnetList = document.getElementById("comment-list");
+  const commentList = document.getElementById('comment-list');
   const currentUid = authService.currentUser.uid;
-  
-  commnetList.innerHTML = "";
+  commentList.innerHTML = '';
   cmtObjList.forEach((cmtObj) => {
     const isOwner = currentUid === cmtObj.creatorId;
     const temp_html = `<div class="card commentCard">
@@ -209,21 +237,25 @@ export const getCommentList = async (time) => {
                     cmtObj.id
                   }" class="noDisplay"><input class="newCmtInput" type="text" maxlength="30" /><button class="updateBtn" onclick="update_comment(event)">완료</button></p>
                   <footer class="quote-footer"><div>BY&nbsp;&nbsp;<img class="cmtImg" width="50px" height="50px" src="${
-                    cmtObj.profileImg ?? "/assets/blankProfile.webp"
+                    cmtObj.profileImg ?? '/assets/blankProfile.webp'
                   }" alt="profileImg" /><span>${
-      cmtObj.nickname ?? "닉네임 없음"
+      cmtObj.nickname ?? '닉네임 없음'
     }</span></div><div class="cmtAt">${cmtObj.createdAt
       .toDate()
       .toLocaleString()}</div></footer>
       <div>
-          <input type="text" value="${cmtObj.plusCounter}" id="input1${cmtObj.id}" />
-          <button onclick="commentLike(event)" class="hate" id="${cmtObj.id}" name="${cmtObj.creatorId}">좋아요</button>
-          <input type="text" value="${cmtObj.minusCounter}" id="input2${cmtObj.id}" />
-          <button onclick="commentHate(event)" class="hate" id="${cmtObj.id}" name="${cmtObj.creatorId}" >싫어요</button>
-      </div>
 
+  <input type="text" value="${cmtObj.plusCounter}" id="input1${cmtObj.id}" />
+  <button onclick="commentLike(event)" class="hate ${cmtObj.likeButton}" id="${
+      cmtObj.id
+    }" name="${cmtObj.creatorId}">좋아요</button>
+  <input type="text" value="${cmtObj.minusCounter}" id="input2${cmtObj.id}" />
+  <button onclick="commentHate(event)" class="hate ${cmtObj.hateButton}"" id="${
+      cmtObj.id
+    }" name="${cmtObj.creatorId}">싫어요</button>
+</div>
               </div>
-              <div class="${isOwner ? "updateBtns" : "noDisplay"}">
+              <div class="${isOwner ? 'updateBtns' : 'noDisplay'}">
                    <button onclick="onEditing(event)" class="editBtn btn btn-dark">수정</button>
                 <button name="${
                   cmtObj.id
@@ -231,31 +263,23 @@ export const getCommentList = async (time) => {
               </div>            
             </div>
      </div>`;
-    const div = document.createElement("div");
-    div.classList.add("mycards");
+    const div = document.createElement('div');
+    div.classList.add('mycards');
     div.innerHTML = temp_html;
-    commnetList.appendChild(div);
-
-    // const hate = document.querySelector(".hate");
-    // console.log(hate);
-    // console.log(currentUid === cmtObj.creatorId);
-    // if((currentUid === cmtObj.creatorId)) {
-    //   console.log(document.querySelector(`button[name=${cmtObj.creatorId}]`));
-    //   document.getElementById(cmtObj.id).disabled = true;
-    // }
-
+    commentList.appendChild(div);
+    console.log(commentList);
   });
 
-  // console.log(document.querySelectorAll(".hate"));
-  document.querySelectorAll(".hate").forEach((el)=>{
-    // console.log(el.name);
-    if((currentUid === el.name)) {
+  document.querySelectorAll('.hate').forEach((el) => {
+    if (currentUid === el.name) {
       el.disabled = true;
     }
-  })  
+
+    if (el.className === `hate ${currentUid}`) {
+      el.disabled = true;
+    }
+  });
 };
-
-
 
 export const getHomePageList = (target) => {
   const { photoURL, displayName } = authService.currentUser;
@@ -283,9 +307,9 @@ export const getHomePageList = (target) => {
       id="profileImg"
       width="50em"
       height="50em"
-      src="${photoURL ?? "/assets/blankProfile.webp"}"
+      src="${photoURL ?? '/assets/blankProfile.webp'}"
     />
-    <span id="nickname">${displayName ?? "닉네임 없음"}</span>
+    <span id="nickname">${displayName ?? '닉네임 없음'}</span>
   </div>
 
   <div class="write-comment__textbox">
@@ -304,12 +328,12 @@ export const getHomePageList = (target) => {
     </button>
   </div>
 </div>`;
-  const wrap = document.querySelector(".wrap");
-  wrap.innerHTML = "";
+  const wrap = document.querySelector('.wrap');
+  wrap.innerHTML = '';
   wrap.innerHTML = temp_html;
-  if (target.textContent === "오늘") selectedDate = "today";
-  else if (target.textContent === "내일") selectedDate = "tomorrow";
-  else selectedDate = "yesterday";
+  if (target.textContent === '오늘') selectedDate = 'today';
+  else if (target.textContent === '내일') selectedDate = 'tomorrow';
+  else selectedDate = 'yesterday';
   getCommentList(selectedDate);
 };
 
@@ -317,7 +341,7 @@ export const getHomePageList = (target) => {
 
 export const getQuestionList = async (index) => {
   let qstObjList = [];
-  const q = query(collection(dbService, "questions"));
+  const q = query(collection(dbService, 'questions'));
 
   const querySnapshot = await getDocs(q);
   querySnapshot.forEach((doc) => {
@@ -328,17 +352,16 @@ export const getQuestionList = async (index) => {
     qstObjList.push(questionObj);
   });
 
-  const commentInput = document.getElementById("commentId");
+  const commentInput = document.getElementById('commentId');
 
   commentInput.value = qstObjList[index].content;
-  console.log(qstObjList[0].content);
 };
 
 //게시물 소개 가져오기
 
 export const getQuestionIntroduce = async (index) => {
   let qstObjList = [];
-  const q = query(collection(dbService, "introtest"));
+  const q = query(collection(dbService, 'introtest'));
 
   const querySnapshot = await getDocs(q);
   querySnapshot.forEach((doc) => {
@@ -348,10 +371,7 @@ export const getQuestionIntroduce = async (index) => {
     qstObjList.push(questionObj);
   });
 
-  const Input = document.getElementById("commentIntroduceId");
+  const Input = document.getElementById('commentIntroduceId');
 
   Input.value = qstObjList[index].intro;
-  console.log(qstObjList[0].intro);
-  console.log(qstObjList[1].intro);
-  console.log(qstObjList[2].intro);
 };
